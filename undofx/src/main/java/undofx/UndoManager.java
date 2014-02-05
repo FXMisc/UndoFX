f09a00eca8cfc340a16d4d9e45ab3e24da7331a3
Tomas Mikula <tomas.mikula@gmail.com>
2014-02-05 22:15:44 +0100

    Add implementation from CodeAreaFX.

---
+++ undofx/src/main/java/undofx/UndoManager.java
@@ -0,0 +1,25 @@
+package undofx;
+
+import javafx.beans.value.ObservableBooleanValue;
+
+public interface UndoManager {
+    boolean undo();
+
+    boolean redo();
+
+    ObservableBooleanValue undoAvailableProperty();
+    boolean isUndoAvailable();
+
+    ObservableBooleanValue redoAvailableProperty();
+    boolean isRedoAvailable();
+
+    /**
+     * Prevents the next change from being merged with the last one.
+     */
+    void preventMerge();
+
+    /**
+     * Stops listening to change events.
+     */
+    void close();
+}
