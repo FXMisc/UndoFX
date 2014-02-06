1312d28ab93e9685fb81466a941d9a8b2e45184f
Tomas Mikula <tomas.mikula@gmail.com>
2014-02-06 17:55:56 +0100

    Improve Javadoc.

--- undofx/src/main/java/undofx/UndoManager.java
+++ undofx/src/main/java/undofx/UndoManager.java
@@ -3,23 +3,38 @@ package undofx;
 import javafx.beans.value.ObservableBooleanValue;
 
 public interface UndoManager {
+    /**
+     * Undo the most recent change, if there is any change to undo.
+     * @return {@code true} if a change was undone, {@code false} otherwise.
+     */
     boolean undo();
 
+    /**
+     * Redo previously undone change, if there is any change to redo.
+     * @return {@code true} if a change was redone, {@code false} otherwise.
+     */
     boolean redo();
 
+    /**
+     * Indicates whether there is a change that can be undone.
+     */
     ObservableBooleanValue undoAvailableProperty();
     boolean isUndoAvailable();
 
+    /**
+     * Indicates whether there is a change that can be redone.
+     * @return
+     */
     ObservableBooleanValue redoAvailableProperty();
     boolean isRedoAvailable();
 
     /**
-     * Prevents the next change from being merged with the last one.
+     * Prevents the next change from being merged with the latest one.
      */
     void preventMerge();
 
     /**
-     * Stops listening to change events.
+     * Stops observing change events.
      */
     void close();
 }
