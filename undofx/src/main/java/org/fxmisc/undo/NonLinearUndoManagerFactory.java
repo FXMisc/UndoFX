488639895ce3644883f052867f39b74245e8065a
Jordan Martinez <jordanalex.martinez@gmail.com>
2016-06-02 20:41:14 -0700

    Add a zero-size variant of non-linear undo manager

--- undofx/src/main/java/org/fxmisc/undo/NonLinearUndoManagerFactory.java
+++ undofx/src/main/java/org/fxmisc/undo/NonLinearUndoManagerFactory.java
@@ -3,6 +3,7 @@ package org.fxmisc.undo;
 import org.fxmisc.undo.impl.nonlinear.DirectedAcyclicGraphImpl;
 import org.fxmisc.undo.impl.nonlinear.NonLinearUndoManagerImpl;
 import org.fxmisc.undo.impl.nonlinear.UnlimitedNonLinearChangeQueue;
+import org.fxmisc.undo.impl.nonlinear.ZeroSizeNonLinearChangeQueue;
 import org.reactfx.EventStream;
 
 import java.util.Optional;
@@ -14,20 +15,25 @@ public interface NonLinearUndoManagerFactory<C> {
 
     DirectedAcyclicGraphImpl<C> getGraph();
 
-    default UndoManager create(
+    default UndoManager createUnlimited(
             EventStream<C> changeStream,
             Function<? super C, ? extends C> invert,
             Consumer<C> apply) {
-        return create(changeStream, invert, apply, (c1, c2) -> Optional.empty());
+        return createUnlimited(changeStream, invert, apply, (c1, c2) -> Optional.empty());
     }
 
-    default UndoManager create(
+    default UndoManager createUnlimited(
             EventStream<C> changeStream,
             Function<? super C, ? extends C> invert,
             Consumer<C> apply,
             BiFunction<C, C, Optional<C>> merge) {
         UnlimitedNonLinearChangeQueue<C> queue = new UnlimitedNonLinearChangeQueue<>(getGraph());
         return new NonLinearUndoManagerImpl<>(queue, invert, apply, merge, changeStream);
+    }
+
+    default UndoManager createZero(EventStream<C> changeStream) {
+        ZeroSizeNonLinearChangeQueue<C> queue = new ZeroSizeNonLinearChangeQueue<>();
+        return new NonLinearUndoManagerImpl<>(queue, c -> c, c -> {}, (c1, c2) -> Optional.empty(), changeStream);
     }
 
 }
